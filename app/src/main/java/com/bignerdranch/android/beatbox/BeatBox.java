1f8d96661699224b359508600629e6017857f611
andro1d <zhezlov@gmail.com>
2017-03-19 22:13:47 +0400

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.java
@@ -0,0 +1,83 @@
+package com.bignerdranch.android.beatbox;
+
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by andro1d on 16.03.2017.
+ */
+
+public class BeatBox {
+    private static final String TAG = "BeatBox";
+
+    private static final String SOUNDS_FOLDER = "sample_sounds";
+    private static final int MAX_SOUNDS = 5;
+
+    private AssetManager mAssets;
+    private List<Sound> mSounds = new ArrayList<>();
+    private SoundPool mSoundPool;
+
+
+    public BeatBox(Context context) {
+        mAssets = context.getAssets();
+
+        mSoundPool = new SoundPool(MAX_SOUNDS, AudioManager.STREAM_MUSIC, 0);
+        loadSounds();
+    }
+
+    public void release(){
+        mSoundPool.release();
+    }
+
+    public void play (Sound sound){
+        Integer soundId = sound.getSoundId();
+        if (soundId ==null){
+            return;
+        }
+        mSoundPool.play(soundId, 1.0f, 1.0f, 1 ,0 , 1.0f);
+    }
+
+    private void loadSounds(){
+        String[] soundsNames;
+
+        try{
+            soundsNames = mAssets.list(SOUNDS_FOLDER);
+            Log.i(TAG, "Found " + soundsNames.length + " sounds");
+        }catch (IOException ioe){
+            Log.e(TAG, "Could not list assest");
+            return;
+        }
+
+        for (String fileName :
+                soundsNames) {
+            try {
+                String assetPath = SOUNDS_FOLDER + "/" + fileName;
+                Sound sound = new Sound(assetPath);
+                load(sound);
+                mSounds.add(sound);
+            } catch (IOException e) {
+                Log.e(TAG, "Could not load sound " + fileName, e);
+            }
+
+        }
+    }
+
+    public List<Sound> getSounds(){
+        return mSounds;
+    }
+
+    private void load(Sound sound) throws IOException{
+        AssetFileDescriptor assetFileDescriptor = mAssets.openFd(sound.getAssetPath());
+        int soundId = mSoundPool.load(assetFileDescriptor, 1);
+        sound.setSoundId(soundId);
+    }
+
+}
